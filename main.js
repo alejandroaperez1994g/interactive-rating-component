17f7df92bc8d7c640ae6b9dadb02dcb26c6a8fcf
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-04-17 23:44:52 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const button = document.querySelector(".submit-button");
+const grades = document.querySelectorAll(".grades");
+const mainPage = document.querySelector(".component-container");
+const thankPage = document.querySelector(".thank-you");
+const valorationText = document.querySelector(".valoration");
+let valoration = 0;
+
+const handleClick = (event) => {
+  event.preventDefault();
+  const grade = event.target;
+  valoration = grade.getAttribute("value");
+  grade.style.backgroundColor = "hsl(25, 97%, 53%)";
+  grade.style.color = "hsl(0, 0%, 100%)";
+  setDefault(event);
+};
+
+const setDefault = (event) => {
+  const father = event.target.parentElement;
+  Array.from(father.children).forEach((child) => {
+    if (child.getAttribute("value") !== valoration) {
+      child.style.backgroundColor = "hsla(213, 19%, 23%, 0.88)";
+      child.style.color = "hsl(216, 12%, 54%)";
+    }
+  });
+};
+
+const handleSubmit = (event) => {
+  if (valoration === 0) return;
+  mainPage.classList.add("hidden");
+  thankPage.classList.remove("hidden");
+  valorationText.innerHTML = `You selected ${valoration} out of 5`;
+};
+
+Array.from(grades).forEach((grade) => {
+  grade.addEventListener("click", handleClick);
+});
+
+button.addEventListener("click", handleSubmit);
